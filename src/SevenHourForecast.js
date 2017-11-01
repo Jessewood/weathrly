b87ab9d3c8c2da3c97b4d6cfaaa21c1bc64af220
Jesse Packwood <packwoodjustin@gmail.com>
2017-10-31 21:16:28 -0600

    make card component

--- src/SevenHourForecast.js
+++ src/SevenHourForecast.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './SevenHourForecast.scss'
+import Card from './Card'
 
 const SevenHourForecast = (props) => {
   return(
     <div className='seven-hour-wrapper'>
       <h2>7 Hour Forecast </h2>
+      <Card />
     </div>
     )
 }
--- src/TenDayForecast.js
+++ src/TenDayForecast.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Card from './Card'
+import './TenDayForecast.scss'
 
 const TenDayForecast = (props) => {
   return(
     <div>
       <h2>10 Day Forecast </h2>
+      <Card />
     </div>
     )
 }
