729a18d2584588f12f4d13b6a51856044afa7868
Jesse Packwood <packwoodjustin@gmail.com>
2017-11-01 15:57:40 -0600

    build click function to toggle app state

--- src/Welcome.js
+++ src/Welcome.js
@@ -2,11 +2,11 @@ import React from 'react'
 import './Welcome.scss'
 import Search from './Search'
 
-const Welcome = () => {
+const Welcome = (props) => {
   return (
     <div  className="welcome">
       <h1 className="welcome-title">Weathrly</h1>
-      <Search />
+      <Search citySearch={props.searchFunction} />
     </div>
   )
 }
