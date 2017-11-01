655c96d82b250f9c4d7117460ce474d482a936fc
julie hawkins <julabi@gmail.com>
2017-11-01 13:34:13 -0600

    adds box-shadow and card sstyling

--- src/CurrentWeather.js
+++ src/CurrentWeather.js
@@ -5,12 +5,14 @@ const CurrentWeather = (props) => {
   return(
     <div className='current-weather-wrapper'>
       <h2 className='current-weather-title'>Current Weather</h2>
-      <p>name of the current city</p>
-      <p>current condition</p>
-      <p>current day</p>
-      <p>current temperature</p>
-      <p>expected high and expected low for the day</p>
-      <p>weather summary(“Windy, chance of a few late night showers. Lows overnight in the mid 50s.)</p>
+      <div className='card'>
+        <p>name of the current city</p>
+        <p>current condition</p>
+        <p>current day</p>
+        <p>current temperature</p>
+        <p>expected high and expected low for the day</p>
+        <p>weather summary(“Windy, chance of a few late night showers. Lows overnight in the mid 50s.)</p>
+      </div>
     </div>
 
     )
