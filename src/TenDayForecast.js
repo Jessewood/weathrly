236310173751b9c4c1c7f220f7e1708f0b7bb2ea
julie hawkins <julabi@gmail.com>
2017-10-31 22:02:35 -0600

    card becomse stateless, 10day becomes stateful and mocks card array

--- src/TenDayForecast.js
+++ src/TenDayForecast.js
@@ -3,13 +3,26 @@ import Card from './Card'
 
 import './TenDayForecast.scss'
 
-const TenDayForecast = (props) => {
-  return(
-    <div className='ten-day-card-wrapper'>
-      <h2>10 Day Forecast </h2>
-      <Card />
-    </div>
+export default class TenDayForecast extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      list: [1,2,3,4,5,6,7,8,9,10]
+    }
+  }
+
+  render() {
+    const cards = []
+    return(
+      <div className='ten-day-card-wrapper'>
+        <h2>10 Day Forecast </h2>
+        {
+          this.state.list.map((card) => {
+            return <Card />
+          }) 
+        }
+      </div>
     )
+  }
 }
 
-export default TenDayForecast
