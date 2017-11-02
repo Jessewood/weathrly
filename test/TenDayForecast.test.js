59d3339a5a61ffb3c490445a2820bced0520a84a
julie hawkins <julabi@gmail.com>
2017-11-02 15:33:49 -0600

    imports sorting-suite and applies quicksort to city array

---
+++ test/TenDayForecast.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import TenDayForecast from '../src/TenDayForecast';
+
+
+describe('TenDayForecast', () => {
+  it('should shallow', () => {
+    // const component = shallow(<TenDayForecast />);
+
+    
+  })
+})
