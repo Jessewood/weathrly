9425ff9375489cb764c37b8524213034c5d18930
julie hawkins <julabi@gmail.com>
2017-11-03 18:57:07 -0600

    scroll bar and suggestions are showing 10 or less cities

--- src/Search.js
+++ src/Search.js
@@ -20,38 +20,44 @@ export default class Search extends Component {
     if (event.target.value.length > 2) {
       this.setState({searchLocation: event.target.value, suggestionsArray: this.trie.suggest(event.target.value)})
     }
+    if (event.target.value.length <=2) {
+      this.setState({suggestionsArray: []})
+    }
   }
 
   renderSuggestions() {
       console.log(this.state.suggestionsArray)
     return (
-      <ul>
-      {this.state.suggestionsArray.map( (suggestion) => {
-        return (<li>{suggestion}</li>)
-        }
-      )}
+      <ul className="suggestion-list" 
+          style={{"height": Math.min(400, this.state.suggestionsArray.length * 40) + "px"}}>
+        {this.state.suggestionsArray.map( (suggestion, suggestIndex) => {
+          return (<li key={suggestIndex}>{suggestion}</li>)
+          }
+        )}
       </ul>
     )
   }
 
   render() {
     return (
-      <div className="search-wrapper">
-        <div className="input-wrapper">
-          <img src="../public/if_location_pin.svg" 
-               className="search-icon" />
-          <input className="search-input" 
-                 value={this.state.value} 
-                 placeholder='Choose a city or zip code...' 
-                 onChange={this.onChange.bind(this)} />
-          {this.renderSuggestions()}
+      <div>
+        <div className="search-wrapper">
+          <div className="input-wrapper">
+            <img src="../public/if_location_pin.svg" 
+                 className="search-icon" />
+            <input className="search-input" 
+                   value={this.state.value} 
+                   placeholder='Choose a city or zip code...' 
+                   onChange={this.onChange.bind(this)} />
+          </div>
+          <button className="search-button"
+                  disabled={!this.state.value}
+                  onClick={() => {
+                    this.props.citySearch(this.state.searchLocation)
+                    this.setState({value: ''})
+                  }}>Search</button>
         </div>
-        <button className="search-button"
-                disabled={!this.state.value}
-                onClick={() => {
-                  this.props.citySearch(this.state.searchLocation)
-                  this.setState({value: ''})
-                }}>Search</button>
+          {this.renderSuggestions()}
       </div>
     )
   }
