31df06a769f50bfccfe424d40fdc525abbad4045
julie hawkins <julabi@gmail.com>
2017-11-04 14:46:56 -0600

    fixed suggestion bug, restyled 404, no more quicksort

--- src/FourOhFour.js
+++ src/FourOhFour.js
@@ -2,14 +2,15 @@ import React from 'react';
 import Search from './Search';
 import './FourOhFour.scss';
 
-const FourOhFour = () => {
+const FourOhFour = (props) => {
+  console.log(props.citySearch);
   return (
     <div className='error-wrapper'>
       <h1>Weathrly</h1>
-      <img src='./public/sad-cloud.png'/>
+      <Search citySearch={props.citySearch}/>
+      <img className="sad-cloud" src='./public/sad-cloud.png'/>
       <h1>404</h1>
       <h2>Oops, City Not Found</h2>
-      <Search />
     </div>
   );
 };
