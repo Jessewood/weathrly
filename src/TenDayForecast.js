a7aa2d9ae6d5a27caf675839c95c9f722d9ffe1e
Jesse Packwood <packwoodjustin@gmail.com>
2017-11-07 10:20:29 -0700

    changed class components to stateless

--- src/TenDayForecast.js
+++ src/TenDayForecast.js
@@ -3,40 +3,32 @@ import Card from './Card';
 import PropTypes from 'prop-types';
 import './TenDayForecast.scss';
 
-export default class TenDayForecast extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-    };
-  }
-
-  render() {
-    const tenDayForecast = this.props.tenDayForecast;
-
-    return (
-      <div className='ten-day-info-wrapper'>
-        <h2>10 Day Forecast</h2>
-        <div className='ten-card-wrapper'>
-        {
-          tenDayForecast.map((dayForecast, dayIndex) => {
-            return (
-              <Card
-                key={dayIndex}
-                date={dayForecast.date}
-                conditions={dayForecast.conditions}
-                icon={dayForecast.icon}
-                high={dayForecast.high}
-                low={dayForecast.low} />
-            );
-          })
-        }
-        </div>
+const TenDayForecast = (props) => {
+  return (
+    <div className='ten-day-info-wrapper'>
+      <h2>10 Day Forecast</h2>
+      <div className='ten-card-wrapper'>
+      {
+        props.tenDayForecast.map((dayForecast, dayIndex) => {
+          return (
+            <Card
+              key={dayIndex}
+              date={dayForecast.date}
+              conditions={dayForecast.conditions}
+              icon={dayForecast.icon}
+              high={dayForecast.high}
+              low={dayForecast.low} />
+          );
+        })
+      }
       </div>
-    );
-  }
+    </div>
+  );
 }
+
 
 TenDayForecast.propTypes = {
   tenDayForecast: PropTypes.array
 };
 
+export default TenDayForecast;
